8c091fb69eaff9cec77e60aa156bb0df708e1175
Phetja <thanachot.sripakdee@gmail.com>
2023-10-16 17:08:52 +0700

    edit BASE_URL.

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
-const BASE_URL = 'http://localhost:4000/api/v1/';
+const BASE_URL = 'https://mern-deploy-backend-9ewg.onrender.com/api/v1/';
 
 const GlobalContext = React.createContext();
 
