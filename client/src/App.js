944c8793914c3c8bbccb57c69f10b1c569cc87f8
Phetja <thanachot.sripakdee@gmail.com>
2023-10-16 14:34:54 +0700

    added backend api for frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch('http://localhost:4000')
+    fetch('https://mern-deploy-backend-ji5i.onrender.com')
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
